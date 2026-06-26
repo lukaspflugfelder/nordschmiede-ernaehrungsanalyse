8720dfe3e6c55e40c361121c8d5ddef8c874da65
Lukas Pflugfelder <DEINE-GITHUB-EMAIL>
2026-06-26 20:19:53 +0200

    Update app layout

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
 export const metadata = {
   title: "Nordschmiede Ernährungsanalyse",
-  description: "KI-gestütztes Ernährungstool",
+  description:
+    "Kostenlose Nordschmiede Ernährungsanalyse: 7 Tage Ernährung eintragen, Ist-Zustand, Soll-Zustand und ersten Umsetzungsschritt erhalten.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="de">
-      <body>{children}</body>
+    <html lang="de" suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
